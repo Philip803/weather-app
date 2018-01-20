c69be7be7f90d363f4ed08d60400d6d820483b12
Sing <sing@Sings-MacBook-Air.local>
2018-01-20 13:07:40 -0500

    Handled the error input address

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const request = require("request"); //install npm request
 const yargs = require("yargs")
 
+// node app.js -a "address to fetch data"
 const argv = yargs.options({
   a: {
     demand: true,
@@ -13,19 +14,27 @@ const argv = yargs.options({
 .alias("help","h")
 .argv;
 
-console.log(argv)
-
+//encode method to replace space in string
 var encodedAddress = encodeURIComponent(argv.address)
-//enter node app.js -a "address to fetch data"
 
-//based on npm doc, follow instruction
+//based on npm doc, followed instruction
 request({
   url : `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true}
   , (error, response,body) => {
     // console.log(JSON.stringify(body,undefined,2)) //print JSON in string
-    console.log(`Address: ${body.results[0].formatted_address}`) //convert it to string to see everything
-    console.log(`Lat: ${body.results[0].geometry.location.lat} `)
-    console.log(`Log: ${body.results[0].geometry.location.lng} `)
+    if(error){
+      console.log("Unable to connect to Google server")
+    } else if (body.status === "ZERO_RESULTS"){
+      console.log("Unable to find that address")
+    } else if (body.status === "OK"){
+      console.log(`Address: ${body.results[0].formatted_address}`) //convert it to string to see everything
+      console.log(`Lat: ${body.results[0].geometry.location.lat} `)
+      console.log(`Log: ${body.results[0].geometry.location.lng} `)
+    } else {
+      console.log("Something went wrong")
+    }
+
+
 
 })
